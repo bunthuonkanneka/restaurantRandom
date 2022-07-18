377d44ae210abccf72d79fdaae324ad883c9722f
Kanneka <bunthuonkanneka@gmail.com>
2022-07-18 15:24:51 +1000

    basic tryout

--- restaurantRandom/ViewController.swift
+++ restaurantRandom/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBAction func button1(_ sender: Any) {
+        label1.text =  "Hello World"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
